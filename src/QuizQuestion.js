cd7b3021c98fc7515de0c9f6c56e36c593c953fd
XD99VU <josja.heerema@ing.com>
2018-11-05 15:13:56 +0100

    Displaying An Error Message Based on State

--- src/QuizQuestion.js
+++ src/QuizQuestion.js
@@ -30,14 +30,15 @@ class QuizQuestion extends Component {
                 <section>
                     <p>{this.props.quiz_question.instruction_text}</p>
                 </section>
-                <section className="buttons">
+                <section
+                 className="buttons">
                     <ul>
                         {this.props.quiz_question.answer_options.map((answer_option, index) => {
                             return <QuizQuestionButton key={index} button_text={answer_option} clickHandler={this.handleClick.bind(this)} />
                         })}
                     </ul>
                 </section>
-                {this.state.incorrectAnswer ? <p class="error">Sorry, that's not right</p> : null}
+                {this.state.incorrectAnswer ? <p className="error">Sorry, that's not right</p> : null}
             </main>
         );
     }
